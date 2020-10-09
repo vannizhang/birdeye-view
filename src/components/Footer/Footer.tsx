0777c2427b806d1d218802552e7d9e3fc9143019
Jinnan Zhang <vannizhang@gmail.com>
2020-10-09 12:08:37 -0700

    add link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,16 +3,19 @@ import FooterTitleImage from '../../statics/FooterTitle.png';
 
 const Footer = () => {
     return (
-        <img 
-        style={{
-            position: 'absolute',
-            width: '100%',
-            // height: '100%',
-            left: 0,
-            bottom: 0
-        }}
-        src={FooterTitleImage} 
-    />
+        <a href="https://www.loc.gov/resource/g3742w.ct005519/?r=0.029,-0.096,0.772,1.256,90" target="_blank">
+            <img 
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    // height: '100%',
+                    left: 0,
+                    bottom: 0
+                }}
+                src={FooterTitleImage} 
+            />
+        </a>
+
     )
 }
 
